9a19954da2746e295350a7d89e0e95ebc6a123c1
sander <sanderantheunis@hotmail.com>
2022-04-21 02:51:12 -0700

    python labo01

---
+++ hello_world.py
@@ -0,0 +1,32 @@
+print("------------------------")     # info field
+print("|    created by Ant    |")
+print("|      hello_world     |")
+print("|   eat, sleep, code   |")
+print("|     copyright TM     |")
+print("------------------------")
+greeting = "hello"                    # de groet
+planet = "world"                      # de planneet
+print(greeting + ' ' + planet)        # samen voegen
+print("# this is not a comment....")  # but this is
+
+print('I\'m OK')               # i'm OK
+print('Hello!\nWorld!')        # Hello!  World!
+print('9\ti\n10\tj')           # 9 tab i, 10 tab j
+print('Hello World! \u262e')   # hello world + peace emoji
+print('Hello \U0001F30D!')     # hello (wereldbol)
+print('Voil\xe0')              # voil + à
+
+print('''I've got this!''')
+print(""""And that", he said, "is all there really is to it.".""")
+print(r'''this newline character isn't one: \n''')
+
+print('this '
+      'string is '
+      'split over '
+      'multiple lines')
+
+hexstr = hex(12648430)
+print(hex(3735928559))
+
+print("High hopes")
+hex(1207)
